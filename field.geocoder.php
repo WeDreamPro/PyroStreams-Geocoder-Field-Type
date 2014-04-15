f32d546f2775a6f4213615e1ddd640fd39cb29af
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-04-15 10:10:21 -0500

    Adding Spanish Lang

--- field.geocoder.php
+++ field.geocoder.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * PyroStreams Geocoder Field Type
@@ -7,114 +9,115 @@
  *
  * @package		PyroStreams
  * @author		David Lewis
+ * @author		Jose Fonseca
  * @copyright	Copyright (c) 2012, David Lewis
  */
 class Field_geocoder {
 
-	public $field_type_slug			= 'geocoder';
+    public $field_type_slug = 'geocoder';
+    public $db_col_type = 'varchar';
+    public $version = '1.0.0';
+    public $author = array('name' => 'David Lewis', 'url' => 'http://jedihorsemanship.com');
 
-	public $db_col_type				= 'varchar';
+    // --------------------------------------------------------------------------
 
-	public $version					= '1.0.0';
+    /**
+     * Output form input
+     *
+     * @access	public
+     * @param $data	array
+     * @return	string
+     */
+    public function form_output($data) {
+        $value = json_decode($data['value']);
+        $options = array(
+            'name' => $data['form_slug'],
+            'id' => $data['form_slug'],
+            'value' => $data['value'],
+            'type' => 'hidden',
+        );
 
-	public $author					= array('name' => 'David Lewis', 'url' => 'http://jedihorsemanship.com');
+        $html = '<span id="' . $data['form_slug'] . '_msg" class="stream_map_msg"></span>';
+        $html .= '<div id="' . $data['form_slug'] . '_map" class="stream_map"></div>';
 
-	// --------------------------------------------------------------------------
+        $options_input = array(
+            'id' => $data['form_slug'] . '_input',
+            // jhubb81 added name attribute to generated address_input to allow for proper javascript validation
+            'name' => $data['form_slug'] . '_input',
+            # Check if addres exists to maintain backward compatibility
+            'value' => (!empty($value->address)) ? $value->address : $data['value'],
+            'type' => 'text',
+        );
 
-	/**
-	 * Output form input
-	 *
-	 * @access	public
-	 * @param $data	array
-	 * @return	string
-	 */
-	public function form_output($data) {
-	  $value = json_decode($data['value']);
-		$options = array(
-			'name'  => $data['form_slug'],
-			'id'    => $data['form_slug'],
-			'value' => $data['value'],
-			'type'  => 'hidden',
-		);
-
-		$html = '<span id="'.$data['form_slug'].'_msg" class="stream_map_msg"></span>';
-		$html .= '<div id="'.$data['form_slug'].'_map" class="stream_map"></div>';
-
-		$options_input = array(
-			'id'    => $data['form_slug'].'_input',
-			// jhubb81 added name attribute to generated address_input to allow for proper javascript validation
-			'name'	=> $data['form_slug'].'_input',
-      # Check if addres exists to maintain backward compatibility
-			'value' => (!empty($value->address)) ? $value->address : $data['value'],
-			'type'  => 'text',
-		);
-
-		return form_input($options).form_input($options_input).$html;
-	}
-
-	// --------------------------------------------------------------------------
-
-	/**
-	 * Tag output variables
-	 *
-	 * Outputs 'latitude' & 'longitude' variables
-	 *
-	 * @access 	public
-	 * @param	string
-	 * @param	array
-	 * @return	array
-	 */
-	public function pre_output_plugin($input) {
-		if ( ! $input) return null;
-
-		$location = json_decode($input);
-
-    # Maintain backward compatability
-		if (!is_object($location)) {
-      $pieces = explode(',', $input);
-      if (count($pieces) != 2) return null;
-
-      $array = array(
-        'lat'     => $pieces[0],
-        'lng'     => $pieces[1],
-        'address' => null,
-      );
-
-      $location = (object) $array;
+        return form_input($options) . form_input($options_input) . $html;
     }
 
-		$data = array(
-		    'latitude'  => $location->lat,
-		    'longitude' => $location->lng,
-		    'lat'       => $location->lat,
-		    'lng'       => $location->lng,
-		    'address'   => $location->address,
-		);
+    // --------------------------------------------------------------------------
 
-		return $data;
-	}
+    /**
+     * Tag output variables
+     *
+     * Outputs 'latitude' & 'longitude' variables
+     *
+     * @access 	public
+     * @param	string
+     * @param	array
+     * @return	array
+     */
+    public function pre_output_plugin($input) {
+        if (!$input)
+            return null;
 
-  /**
-   * Event
-   *
-   * Load assets
-   *
-   * @access public
-   * @param $field object
-   * @return void
-   */
-   public function event($field) {
+        $location = json_decode($input);
+
+        # Maintain backward compatability
+        if (!is_object($location)) {
+            $pieces = explode(',', $input);
+            if (count($pieces) != 2)
+                return null;
+
+            $array = array(
+                'lat' => $pieces[0],
+                'lng' => $pieces[1],
+                'address' => null,
+            );
+
+            $location = (object) $array;
+        }
+
+        $data = array(
+            'latitude' => $location->lat,
+            'longitude' => $location->lng,
+            'lat' => $location->lat,
+            'lng' => $location->lng,
+            'address' => $location->address,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Event
+     *
+     * Load assets
+     *
+     * @access public
+     * @param $field object
+     * @return void
+     */
+    public function event($field) {
         $this->CI->type->add_misc('<script src="//maps.google.com/maps/api/js?sensor=false"></script>');
         $this->CI->type->add_js('geocoder', 'geocoder.js');
         $this->CI->type->add_css('geocoder', 'geocoder.css');
         $this->CI->type->add_misc('<script type="text/javascript">
 $(document).ready(function() {
-    initialize("'.$field->field_slug.'");
+    initialize("' . $field->field_slug . '");
     $(".tabs").bind("tabsshow", function(event, ui) {
         google.maps.event.trigger(map, "resize");
         mapLocation();
     });
 });
 </script>');
-        }
+    }
+
 }
--- language/english/geocoder_lang.php
+++ language/english/geocoder_lang.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
 
-$lang['streams:geocoder.name'] 				= 'Geocoder';
-$lang['streams:geocoder.geocoder_error']    = 'Geocode failed for the following reason';
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:geocoder.name'] = 'Geocoder';
+$lang['streams:geocoder.geocoder_error'] = 'La geocodificación fallo por algun motivo';
